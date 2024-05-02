760665e748e6280bfd9798f3bcc18b8c4731974d
pavithrayadava <pavithra10.yadava@gmail.com>
2024-05-02 20:43:12 +0530

    added

--- First.py
+++ First.py
@@ -2,6 +2,8 @@ name = "Andrew Venis"
 branch = "Computer Science"  
 age = "25"  
 print("My name is: ", name, )  
+print("My name is: ", name, )  
+print("My name is: ", name, )  
 print("My age is: ", age, )
 # print("My branch is: ", branch) 
 
